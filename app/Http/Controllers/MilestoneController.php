adb4543e790db8161930b466b55014410f5b0ec8
Izzat Syahmi <syahmiizzat550@gmail.com>
2024-12-17 22:21:32 +0800

    Add controllers and models for Academician, Grant, and Milestone resources with initial migration files

---
+++ app/Http/Controllers/MilestoneController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Milestone;
+use Illuminate\Http\Request;
+
+class MilestoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Milestone $milestone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Milestone $milestone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Milestone $milestone)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Milestone $milestone)
+    {
+        //
+    }
+}
