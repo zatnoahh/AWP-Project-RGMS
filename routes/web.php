8ef08793dbdaddc589716db3ef6fac5d11aa2b5d
Izzat Syahmi <syahmiizzat550@gmail.com>
2024-12-20 22:22:47 +0800

    adding routes for the controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GrantController;
+use App\Http\Controllers\MilestoneController;
+use App\Http\Controllers\AcademicianController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +12,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/grants', GrantController::class);
+Route::resource('/milestones', MilestoneController::class);
+Route::resource('/academicians', AcademicianController::class);
+
