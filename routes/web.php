ea78b781317c23518c1d9f9f191098c787ff2b6d
Izzat Syahmi <syahmiizzat550@gmail.com>
2025-01-08 15:42:33 +0800

    Minor changes

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,24 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/settings', [App\Http\Controllers\SettingsController::class, 'index'])->name('settings.index');
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('/grants', GrantController::class);
-Route::resource('/milestones', MilestoneController::class);
-Route::resource('/academicians', AcademicianController::class);
+    Route::resource('/grants', GrantController::class);
+    Route::resource('/milestones', MilestoneController::class);
+    Route::resource('/academicians', AcademicianController::class);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/error403', function () {
-    return view('errors.403')
-        ->with('error', '403')
-        ->with('message', 'You are not authorized to access this page.');
+    Route::get('/error403', function () {
+        return view('errors.403')
+            ->with('error', '403')
+            ->with('message', 'You are not authorized to access this page.');
+    });
+
+    //Route::get('/calendar/milestones', [CalendarController::class, 'milestones'])->name('calendar.milestones');
+    Route::get('/projects', [App\Http\Controllers\ProjectController::class, 'index'])->name('projects.index');
+    Route::resource('users', UserController::class);
+
 });
 
-Route::get('/calendar/milestones', [CalendarController::class, 'milestones'])->name('calendar.milestones');
 
-
-Route::get('/projects', [App\Http\Controllers\ProjectController::class, 'index'])->name('projects.index');
-});
-
-Route::resource('users', UserController::class);
